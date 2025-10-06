d5a865a838b14c61707af71b707ce886b0969112
JG <joacogilp@gmail.com>
2025-10-06 10:09:02 -0300

    PersistenceManager implemented

--- src/main/java/com/github/JoacoGDev/idgenerator/PersistenceManager.java
+++ src/main/java/com/github/JoacoGDev/idgenerator/PersistenceManager.java
@@ -2,47 +2,97 @@ package com.github.JoacoGDev.idgenerator;
 
 import java.io.*;
 import java.util.*;
-import java.nio.file.*;
 
+
+
+/**
+ * Manages persistence of ID counters to a Properties file.
+ * Stores counters in user's home directory
+ */
+public class PersistenceManager {
+
+    private final String filePath;
 
 
     /**
-     * Manages persistence of ID counters to a Properties file.
-     * Stores counters in user's home directory
+     * Creates a new PersistenceManager
+     * File location: user.home/.idgenerator/counters.properties
      */
-    public class PersistenceManager {
 
-        private final String filePath;
+    public PersistenceManager() {
+        String userHome = System.getProperty("user.home");
 
+        this.filePath = userHome + File.separator + ".idGenerator" +
+                File.separator + "counters.properties";
+    }
 
-        /**
-         * Creates a new PersistenceManager
-         * File location: user.home/.idgenerator/counters.properties
-         */
+    /**
+     * Checks if the persistence file exists
+     *
+     * @return true if file exists, false otherwise
+     */
+    public boolean exists() {
+        File file = new File(filePath);
+        return file.exists();
+    }
 
-        public PersistenceManager(){
-            String userHome = System.getProperty("user.home");
+    /**
+     * Creates the .idgenerator if it doesn't exist.
+     *
+     * @throws IOException if directory cannot be created
+     */
+    private void ensureDirectoryExists() throws IOException {
+        File directory = new File(System.getProperty("user.home") +
+                File.separator + ".idgenerator");
 
-            this.filePath = userHome + File.pathSeparator + ".idGenerator" +
-                            File.separator + "counters.properties";
-        }
-
-        /**
-         * Checks if the persistence file exists
-         * @return true if file exists, false otherwise
-         */
-        public boolean exists() {
-            File file = new File(filePath);
-            return file.exists();
-        }
-
-        /**
-         * Loads counters from properties file.
-         * @return Map of counter names to their values
-         * @throws IOException if file cannot be read
-         */
-        public Map<String, Integer> load() throws IOException{
-            //
-        return;
+        if (!directory.exists()) {
+            throw new IOException("Could not create directory: " + directory);
         }
     }
+
+
+    /**
+     * Loads counters from properties file.
+     *
+     * @return Map of counter names to their values
+     * @throws IOException if file cannot be read
+     */
+    public Map<String, Integer> load() throws IOException {
+        Map<String, Integer> counters = new HashMap<>();
+
+        //Return empty Map if file doesn't exists
+        if (!exists()) {
+            return counters;
+        }
+
+        //Reads file using Properties
+        Properties properties = new Properties();
+        //This line autocloses the resource. PREVENTS RESOURCE LEAKS
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            properties.load(fis);
+        }
+
+        //Transforms Properties into a Map<String, Integer>
+        for (String key : properties.stringPropertyNames()) {
+            String value = properties.getProperty(key);
+            counters.put(key, Integer.parseInt(value));
+        }
+
+        return counters;
+    }
+
+
+    public void save(Map<String, Integer> counters) throws IOException {
+
+        ensureDirectoryExists();
+
+        Properties properties = new Properties();
+        for (Map.Entry<String, Integer> entry : counters.entrySet()) {
+            properties.setProperty(entry.getKey(), entry.getValue().toString());
+        }
+
+        try (FileOutputStream fos = new FileOutputStream(filePath)) {
+            properties.store(fos, "Id Generator Counters - DO NOT EDIT MANUALLY");
+        }
+    }
+}
